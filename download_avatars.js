f5fac668dabff97f6067a01c8b9e46c370734fa2
Mark Barlescu <barlescu92@gmail.com>
2019-01-17 15:08:21 -0800

    updated gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,1 +1,10 @@
 var request = require('request');
+
+function getRepoContributors(repoOwner, repoName, cb){
+  let url = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors()
