53a5831afbf5692a5f6da973580d6e2ba6d74946
Mark Barlescu <barlescu92@gmail.com>
2019-01-17 17:10:23 -0800

    practice with require and fs

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,39 @@
+let token = require('./secrets')
 var request = require('request');
+var fs = require('fs');
+
+var myargs = process.argv.slice(2);
 
 function getRepoContributors(repoOwner, repoName, cb){
-  let url = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`
-  request(url, function(err, res, body) {
+  let options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request',
+      authentication: token.GITHUB_TOKEN,
+    }
+  }
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
-getRepoContributors()
+getRepoContributors(myargs[0], myargs[1], function(err, result) {
+  let images = []
+  console.log("Errors:", err);
+  console.log("Result:", JSON.parse(result).map(function(each) {
+    images.push(each.avatar_url)
+    downloadImageByURL(each.avatar_url, `./avatars/${each.login}.jpg`)
+  }));
+  console.log('images', images)
+});
+
+function downloadImageByURL(url, filePath){
+  request.get(url)               // Note 1
+   .on('error', function (err) {                                   // Note 2
+     throw err;
+   })
+   .on('response', function (response) {                           // Note 3
+     console.log('Response Status Code: ', response.statusCode);
+   })
+   .pipe(fs.createWriteStream(filePath));
+}
